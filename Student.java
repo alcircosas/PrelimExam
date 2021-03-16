768a3695c9dc3be6f84251e32c66eee5693446ff
alcircosas <s1920561@usls.edu.ph>
2021-03-16 15:06:19 +0800

    Initial Student

---
+++ Student.java
@@ -0,0 +1,59 @@
+package Exam;
+import java.util.Scanner;
+class Student {
+	   private String ID;
+	   private int numberOfCredits;
+	   private int points;
+	   private double gradePoint;
+
+	   public Student() {
+	   }
+
+	   // constructor to initialize the values
+	   public Student(String aID, int aNumberOfCredits, int aPoints) {
+	       super();
+	       ID = aID;
+	       numberOfCredits = aNumberOfCredits;
+	       points = aPoints;
+	       calculateGradePoint();
+	   }
+
+	   // getters and setters
+	   public String getID() {
+	       return ID;
+	   }
+
+	   public void setID(String aID) {
+	       ID = aID;
+	   }
+
+	   public int getNumberOfCredits() {
+	       return numberOfCredits;
+	   }
+
+	   public void setNumberOfCredits(int aNumberOfCredits) {
+	       numberOfCredits = aNumberOfCredits;
+	   }
+
+	   public int getPoints() {
+	       return points;
+	   }
+
+	   public void setPoints(int aPoints) {
+	       points = aPoints;
+	   }
+
+	   // to return object in String representation form
+	   @Override
+	   public String toString() {
+	       return "ID : " + ID + " NumberOfCredits : " + numberOfCredits + " Points : " + points + " Grade Point : "
+	               + gradePoint;
+	   }
+
+	   // method to find gradePoint
+	   public void calculateGradePoint() {
+	       gradePoint = points / (double) numberOfCredits;
+	   }
+	}
+
+
